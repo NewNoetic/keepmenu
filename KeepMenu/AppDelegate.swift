4b75b8766b777f0dbe654ad2de638824329df4a5
sdgandhi <sdgandhi@users.noreply.github.com>
2020-04-10 06:15:12 -0700

    Initial Commit

---
+++ KeepMenu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeepMenu
+//
+//  Created by Sidhant Gandhi on 4/10/20.
+//  Copyright © 2020 newnoetic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KeepMenu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeepMenu
+//
+//  Created by Sidhant Gandhi on 4/10/20.
+//  Copyright © 2020 newnoetic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
